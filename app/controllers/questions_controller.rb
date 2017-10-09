b68092d23a492c4b047d6425d6bcf1e93b74203d
junggunlee <leejunggun7@gmail.com>
2017-10-09 14:05:28 +0900

    make question ans ask

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+class QuestionsController < ApplicationController
+  def answer
+    @question = params[:query]
+    # raise
+    if @question == "I am going to work right now!"
+      @answer = "..."
+    elsif @question[-1] == "?"
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+
+  def ask
+  end
+end
