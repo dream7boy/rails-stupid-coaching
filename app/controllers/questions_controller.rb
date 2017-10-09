125a718f9854499b4b83dd1909d9fadc111dfafa
junggunlee <leejunggun7@gmail.com>
2017-10-09 14:31:22 +0900

    make UPCASE

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,16 +1,34 @@
 class QuestionsController < ApplicationController
   def answer
     @question = params[:query]
-    # raise
-    if @question == "I am going to work right now!"
+    coach_answer_enhanced(@question)
+  end
+
+  def ask
+  end
+
+  def coach_answer(your_message)
+    if your_message == "I am going to work right now!"
       @answer = "..."
-    elsif @question[-1] == "?"
+    elsif your_message[-1] == "?"
       @answer = "Silly question, get dressed and go to work!"
     else
       @answer = "I don't care, get dressed and go to work!"
     end
   end
 
-  def ask
+  def coach_answer_enhanced(your_message)
+    if your_message == ""
+      coach_answer(your_message)
+    elsif your_message == your_message.upcase
+      if your_message == "I am going to work right now!".upcase
+        @answer = "I can feel your motivation! ..."
+      else
+        @answer = "I can feel your motivation! #{coach_answer(your_message)}"
+      end
+    else
+      coach_answer(your_message)
+    end
   end
 end
+
